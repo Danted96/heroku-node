0890077aa7066cac87234e2644557fa8616a5a9f
Danted96 <dante.domizi@studenti.unicam.it>
2017-12-20 16:45:04 +0100

    change

--- views/server.js
+++ views/server.js
@@ -3,13 +3,13 @@ var app = express();
 
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
-var port = process.env.PORT || 8080;
-
+app.set('port', (process.env.PORT || 8080));
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
 // make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/public'));
+
 
 // set the home page route
 app.get('/', function(req, res) {
@@ -18,6 +18,6 @@ app.get('/', function(req, res) {
     res.render('index');
 });
 
-app.listen(port, function() {
-    console.log('Our app is running on http://localhost:' + port);
+app.listen(app.get('port'), function() {
+    console.log('Node server is running on port ' + app.get('port'));
 });
